398e832c4069607be1bb1b3651aa5f0dea193e06
Ciuff4 <gasperini_cristina@libero.it>
2022-04-07 16:58:49 +0200

    hamburger menu removed by clicking on the X

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const burgerMenu= document.querySelector(".hamburger-menu")
 const burgerLogo= document.querySelector(".fa-bars")
+const burgerX= document.querySelector(".close")
+
 
 burgerLogo.addEventListener("click", function(){
     burgerMenu.classList.add("active")
+});
+
+burgerX.addEventListener("click", function () {
+    burgerMenu.classList.remove("active")    
 });
